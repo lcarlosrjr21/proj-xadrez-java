e90d09a781c76c9a5951d26a8f39956d0f69792c
Luiz Carlos R. Junior <lcarlosrjr@gmail.com>
2020-09-18 15:44:01 -0300

    Retomando ajustes - Melhoria imprimir o tabuleiro

--- src/aplicacao/Principal.java
+++ src/aplicacao/Principal.java
@@ -1,11 +1,10 @@
 package aplicacao;
-
 import xadrez.PartidaXadrez;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		
+
 		PartidaXadrez partidaXadrez = new PartidaXadrez();
 		UI.imprimeTabuleiro(partidaXadrez.getPecas());
 
--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -32,8 +32,10 @@ public class PartidaXadrez {
 		
         /*lugarNovaPeca('b', 1, new Cavalo(tabuleiro, Cor.BRANCO));
         lugarNovaPeca('c', 1, new Bispo(tabuleiro, Cor.BRANCO));
-        lugarNovaPeca('d', 1, new Rainha(tabuleiro, Cor.BRANCO));
-        lugarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO, this));
+        lugarNovaPeca('d', 1, new Rainha(tabuleiro, Cor.BRANCO));*/
+        
+        lugarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+        /*
         lugarNovaPeca('f', 1, new Bispo(tabuleiro, Cor.BRANCO));
         lugarNovaPeca('g', 1, new Cavalo(tabuleiro, Cor.BRANCO));*/
         
@@ -53,7 +55,11 @@ public class PartidaXadrez {
         /*lugarNovaPeca('b', 8, new Cavalo(tabuleiro, Cor.PRETO));
         lugarNovaPeca('c', 8, new Bispo(tabuleiro, Cor.PRETO));
         lugarNovaPeca('d', 8, new Rainha(tabuleiro, Cor.PRETO));
-        lugarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO, this));
+        */
+        
+        lugarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
+        
+        /*
         lugarNovaPeca('f', 8, new Bispo(tabuleiro, Cor.PRETO));
         lugarNovaPeca('g', 8, new Cavalo(tabuleiro, Cor.PRETO));*/
         
